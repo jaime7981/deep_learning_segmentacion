fe3faeda30fadbe18ecac56dec4487985a896a8a
jaime7981 <jmfernandez1@miuandes.cl>
2023-11-11 15:53:02 -0300

    running code with epoch and batches

--- load_dataset.py
+++ load_dataset.py
@@ -6,20 +6,20 @@ BASE_PATH = os.path.dirname(os.path.abspath(__file__))
 DATASET_PATH = os.path.join(BASE_PATH, 'datasets')
 
 
-def load_dataset():
-    datagen_kwargs = dict (rescale = 1./255 , validation_split = 20)
+def load_dataset(batch_size=32):
+    datagen_kwargs = dict(rescale=1./255, validation_split=.20)
     image_datagen = tf.keras.preprocessing.image.ImageDataGenerator(**datagen_kwargs)
     mask_datagen = tf.keras.preprocessing.image.ImageDataGenerator(**datagen_kwargs)
 
-    images_path = os.path.join(DATASET_PATH, 'images')
-    masks_path = os.path.join(DATASET_PATH, 'masks')
+    images_path = os.path.join(DATASET_PATH, 'Images')
+    masks_path = os.path.join(DATASET_PATH, 'Masks')
 
     train_image_generator = image_datagen.flow_from_directory(
         images_path,
         target_size=(128, 128),
         color_mode='grayscale',
         class_mode=None,
-        batch_size=32,
+        batch_size=batch_size,
         shuffle=True,
         seed=42,
         subset='training'
@@ -30,7 +30,7 @@ def load_dataset():
         target_size=(128, 128),
         color_mode='grayscale',
         class_mode=None,
-        batch_size=32,
+        batch_size=batch_size,
         shuffle=True,
         seed=42,
         subset='training'
@@ -41,7 +41,7 @@ def load_dataset():
         target_size=(128, 128),
         color_mode='grayscale',
         class_mode=None,
-        batch_size=32,
+        batch_size=batch_size,
         shuffle=True,
         seed=42,
         subset='validation'
@@ -52,13 +52,16 @@ def load_dataset():
         target_size=(128, 128),
         color_mode='grayscale',
         class_mode=None,
-        batch_size=32,
+        batch_size=batch_size,
         shuffle=True,
         seed=42,
         subset='validation'
     )
 
+    steps_per_epoch = len(train_image_generator)
+    validation_steps = len(test_image_generator)
+
     train_generator = zip(train_image_generator, train_mask_generator)
     test_generator = zip(test_image_generator, test_mask_generator)
 
-    return train_generator, test_generator
+    return train_generator, test_generator, steps_per_epoch, validation_steps
--- main.py
+++ main.py
@@ -4,34 +4,59 @@ from loss_functions import bce_dice_loss, dice_coef
 from load_dataset import load_dataset
 from keras_unet_collection import models
 
+NUM_EPOCHS = 32
+BATCH_SIZE = 10
+
 def main():
+    train_generator, test_generator, steps_per_epoch, validation_steps = load_dataset(batch_size=BATCH_SIZE)
 
-    train_generator, test_generator = load_dataset()
-
+    # U-Net Model
     unet = models.unet_2d(
         input_size=(128, 128, 1),
         filter_num=[64, 128, 256, 512],
         n_labels=1
     )
 
+    unet.compile(
+        optimizer=Adam(),
+        loss=bce_dice_loss,
+        metrics=[dice_coef]
+    )
+
+    # Train U-Net Model
+    unet.fit(
+        train_generator,
+        epochs=NUM_EPOCHS,
+        validation_data=test_generator,
+        steps_per_epoch=steps_per_epoch,
+        validation_steps=validation_steps
+    )
+
+    unet.save('unet.h5')
+
+    # U-Net++ Model
     unet_plus = models.unet_plus_2d(
         input_size=(128, 128, 1),
         filter_num=[64, 128, 256, 512],
         n_labels=1
     )
     
-    unet_train = unet.compile(
-        optimizer=Adam,
+    unet_plus.compile(
+        optimizer=Adam(),
         loss=bce_dice_loss,
         metrics=[dice_coef]
     )
 
-    unet_plus_train = unet_plus.compile(
-        optimizer=Adam,
-        loss=bce_dice_loss,
-        metrics=[dice_coef]
+    # Train U-Net++ Model
+    unet_plus.fit(
+        train_generator,
+        epochs=NUM_EPOCHS,
+        validation_data=test_generator,
+        steps_per_epoch=steps_per_epoch,
+        validation_steps=validation_steps
     )
 
+    unet_plus.save('unet_plus.h5')
 
 if __name__ == '__main__':
     main()
